ceb0ca6465fb70356c04c4158490822034fb6fb9
whoyoux <48690153+whoyoux@users.noreply.github.com>
2024-03-10 22:31:37 +0100

    added key

--- src/components/technology-stack.tsx
+++ src/components/technology-stack.tsx
@@ -4,8 +4,16 @@ import { Technology } from "@/types/types";
 const TechnologyStack = ({ stack }: { stack: Technology[] }) => {
 	return (
 		<div className="flex items-center gap-2 w-full flex-wrap">
-			{stack.map((tech) => (
-				<Badge variant="secondary">{tech}</Badge>
+			{stack.map((tech, idx) => (
+				<Badge
+					variant="secondary"
+					key={`${tech}_${
+						// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+						idx
+					}`}
+				>
+					{tech}
+				</Badge>
 			))}
 		</div>
 	);
